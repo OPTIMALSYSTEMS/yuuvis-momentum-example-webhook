65c8f79785470210b1a5d040dcb80c6cc2e65a87
Michael Zehrer <zehrer@zepan.net>
2020-04-29 13:55:48 +0200

    Add license

--- src/main/java/com/os/services/webhook/rest/MetadataDocumentImportWebhookRestController.java
+++ src/main/java/com/os/services/webhook/rest/MetadataDocumentImportWebhookRestController.java
@@ -1,20 +1,31 @@
 
+/*
+ * Copyright (c) 2020 OPTIMAL SYSTEMS GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.os.services.webhook.rest;
 
-import java.util.Map;
-
+import com.os.services.webhook.service.InsertDocumentService;
+import com.os.services.webhook.service.IsMetadataExistsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.os.services.webhook.service.InsertDocumentService;
-import com.os.services.webhook.service.IsMetadataExistsService;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
--- src/main/java/com/os/services/webhook/service/InsertDocumentService.java
+++ src/main/java/com/os/services/webhook/service/InsertDocumentService.java
@@ -1,14 +1,29 @@
 
+/*
+ * Copyright (c) 2020 OPTIMAL SYSTEMS GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.os.services.webhook.service;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.annotation.PostConstruct;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class InsertDocumentService
